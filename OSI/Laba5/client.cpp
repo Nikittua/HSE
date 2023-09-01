331c34e6020f43beefc6482e7e9e861ee8fc2349
Nikittua <nisergeev_1@edu.hse.ru>
2023-09-02 00:13:20 +0300

    Добавлены комментарии к коду

--- OSI/Laba5/client.cpp
+++ OSI/Laba5/client.cpp
@@ -5,32 +5,31 @@
 
 #define MSGSZ     128
 
-
 struct msgbuf {
     long mtype;
     char mtext[MSGSZ];
 } message_buf;
 
-
 main()
 {
-    int msqid;
-    key_t key;
-    strmessage_buf  rbuf;
+    int msqid;                  // Идентификатор очереди сообщений
+    key_t key;                   // Ключ для доступа к очереди
+    struct msgbuf rbuf;          // Структура для приема сообщения
 
-    key = 10;
+    key = 10;                    // Устанавливаем ключ для доступа к очереди
 
+    // Получаем доступ к существующей очереди сообщений
     if ((msqid = msgget(key, 0666)) < 0) {
-        perror("msgget");
+        perror("msgget");        // Выводим сообщение об ошибке, если не удается получить доступ к очереди
         exit(1);
     }
 
-    
+    // Получаем сообщение из очереди с типом 1
     if (msgrcv(msqid, &rbuf, MSGSZ, 1, 0) < 0) {
-        perror("msgrcv");
+        perror("msgrcv");        // Выводим сообщение об ошибке, если не удается получить сообщение
         exit(1);
     }
 
-    printf("%s\n", rbuf.mtext);
+    printf("%s\n", rbuf.mtext); // Выводим текст полученного сообщения
     exit(0);
 }

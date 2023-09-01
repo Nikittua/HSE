3b7bdbe4911e4b078795cb58ff5a4e566ffccedd
Nikittua <nisergeev_1@edu.hse.ru>
2023-09-02 01:41:39 +0300

    Изменены данные для записи в файл

--- OSI/Laba5/get_pid.cpp
+++ OSI/Laba5/get_pid.cpp
@@ -5,7 +5,7 @@
 
 int main() {
     // Выполняем команду ps с опциями для получения nice value и времени работы
-    FILE* pipe = popen("ps -eo ni,etime", "r");
+    FILE* pipe = popen("ps -eo ni,etime,cmd", "r");
     if (!pipe) {
         std::cerr << "Ошибка при выполнении команды ps." << std::endl;
         return 1;
@@ -18,15 +18,17 @@ int main() {
     while (fgets(buffer, sizeof(buffer), pipe) != NULL) {
         int niceValue;
         char etime[128];
+        char cmd[128];
         // Используем sscanf для извлечения nice value и времени работы
-        if (sscanf(buffer, "%d %s", &niceValue, etime) == 2) {
+        if (sscanf(buffer, "%d %s [%[^]]]", &niceValue, etime, cmd) == 3) {
             if (niceValue > 10) {
                 // Записываем nice value и время работы процессов с niceValue > 30 в файл
-                outfile << "Nice: " << niceValue << "\n" << "Time: " << etime << std::endl;
+                outfile << "Proccess:" << cmd << "\n" << "Time:" << etime << std::endl;
+                printf("File was changed \n");
+
             }
         }
     }
-
     // Закрываем файл и pipe
     outfile.close();
     pclose(pipe);

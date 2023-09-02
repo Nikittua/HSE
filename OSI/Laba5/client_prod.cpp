f188e05a29c3af5d980c3316830b16f55665fc50
Nikittua <nisergeev_1@edu.hse.ru>
2023-09-02 14:54:44 +0300

    Prod Client Ver 0.1

---
+++ OSI/Laba5/client_prod.cpp
@@ -0,0 +1,32 @@
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h> // Для работы с временем
+#include "message_utils.h" // Включаем наш заголовочный файл
+
+int main() {
+    int msqid;                  // Идентификатор очереди сообщений
+    key_t key;                   // Ключ для доступа к очереди
+
+    key = 10;                    // Устанавливаем ключ для доступа к очереди
+
+    // Получаем доступ к существующей очереди сообщений
+    if ((msqid = msgget(key, 0666)) < 0) {
+        perror("msgget");        // Выводим сообщение об ошибке, если не удается получить доступ к очереди
+        exit(1);
+    }
+
+    // Получаем время последнего чтения и выводим его
+    time_t lastReadTime = getLastReadTime(msqid);
+    printf("Время последнего чтения из очереди: %s", ctime(&lastReadTime));
+
+    // Получаем число сообщений в очереди и выводим его
+    int numMessages = getNumMessages(msqid);
+    printf("Число сообщений в очереди: %d\n", numMessages);
+
+    exit(0);
+}
+

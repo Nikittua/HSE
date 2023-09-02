ff0467a19b3fcf32d3f67c1ba39ca56635c07f11
Nikittua <nisergeev_1@edu.hse.ru>
2023-09-02 14:56:30 +0300

    Чтение и вывод сообщения

---
+++ OSI/Laba5/read_msg.cpp
@@ -0,0 +1,28 @@
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h> // Для работы с временем
+#include "message_utils.h" // Включаем наш заголовочный файл
+
+int main() {
+    int msqid;                  // Идентификатор очереди сообщений
+    key_t key;                   // Ключ для доступа к очереди
+    struct my_msgbuf rbuf;       // Структура для получения сообщения
+
+    key = 10;                    // Устанавливаем ключ для доступа к очереди
+
+    // Получаем доступ к существующей очереди сообщений
+    if ((msqid = msgget(key, 0666)) < 0) {
+        perror("msgget");        // Выводим сообщение об ошибке, если не удается получить доступ к очереди
+        exit(1);
+    }
+
+    receiveMessage(msqid, &rbuf);
+    printf("Полученное сообщение: %s\n", rbuf.mtext);
+    
+    exit(0);
+}
+

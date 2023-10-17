e9049338ed425f833dc1d4c75dea5cb41a58273b
Nikittua <nisergeev_1@edu.hse.ru>
2023-10-17 22:36:56 +0300

    test1 Ver 0.6

--- OSI/Laba6/client_prod.cpp
+++ OSI/Laba6/client_prod.cpp
@@ -30,11 +30,11 @@ void listELFExecutablesWithTime(char *output) {
         }
 
         // Get file creation time
-        struct stat file_stat;
-        if (stat(result, &file_stat) == -1) {
-            perror("stat");
-            exit(1);
-        }
+        // struct stat file_stat;
+        // if (stat(result, &file_stat) == -1) {
+        //     perror("stat");
+        //     exit(1);
+        // }
 
         sprintf(output + strlen(output), "%s (Created at: %s)\n", result, ctime(&file_stat.st_ctime));
     }
--- OSI/Laba6/server_prod.cpp
+++ OSI/Laba6/server_prod.cpp
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <dirent.h>
 #include <sys/stat.h>
 #include <unistd.h>
 #include <sys/shm.h>

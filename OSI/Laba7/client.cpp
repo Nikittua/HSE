487f182a70664f37ab0c61273f95bdc3022c3334
Nikittua <nisergeev_1@edu.hse.ru>
2023-12-05 22:22:44 +0300

    fix(client): fixed ps script and added bind

--- OSI/Laba7/client.cpp
+++ OSI/Laba7/client.cpp
@@ -8,45 +8,54 @@
 
 int main() {
     int sock_id, ret;
-    struct sockaddr_in server;
+    struct sockaddr_in addr, server;
     char buf[3000];
     unsigned server_size;
     FILE *pipe;
 
-
-    sock_id = socket(AF_INET, SOCK_DGRAM, 0); //создаем дискриптор сокета домена интернета IPv4 с типом сокета 
-                                           //SOCK_DGRAM. 0 ставим чтобы выбрать протокол
-                                           // по умолчанию для данного домена и типа сокета   
+    sock_id = socket(AF_INET, SOCK_DGRAM, 0);
     if (sock_id < 0) {
         perror("Failed to get sock_id");
         exit(1);
     }
 
-    server.sin_family = AF_INET; // указывам домен
-    server.sin_addr.s_addr = INADDR_ANY; // адрес IPv4 INADDR_ANY - возможность принимать пакеты с любого сетевого интерфейса, установленного в системе
-    server.sin_port = htons(6969); // порт взяли из преобразования аппаратного(host) в сетевой(network) s (short)
+    // Явная привязка клиентского сокета к порту 0
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_port = htons(0);
+
+    ret = bind(sock_id, (struct sockaddr*)&addr, sizeof(addr));
+    if (ret < 0) {
+        perror("Failed to bind sock_id");
+        exit(1);
+    }
+
+    // Затем устанавливаем адрес сервера и порт
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY; // Замените на реальный IP-адрес сервера
+    server.sin_port = htons(6969);
 
     server_size = sizeof(server);
-    std::cout << "client_size " << client_size << std::endl;
+    std::cout << "server_size " << server_size << std::endl;
 
-    pipe = popen("ps -u root | awk '{print $1}' | sed '1d'", "r"); // записываем в pipe результат выполнения команды
-    ret = fread(buf, 1, sizeof(buf), pipe);// возвращает количество действительно считанных объектов 1= 1 байт
+    pipe = popen("ps -u root -o pid,ppid,cmd | awk '$2 == 0 || $2 == 1 || $2 == 2 {print $1}'", "r");
+    ret = fread(buf, 1, sizeof(buf), pipe);
 
-    ret = sendto(sock_id, buf, ret, 0, (struct sockaddr*)&server, server_size); // отправляем серверу количество байтов ret в buf.
+    ret = sendto(sock_id, buf, ret, 0, (struct sockaddr*)&server, server_size);
     if (ret < 0) {
         perror("Failed to send data");
         exit(1);
     }
-    
 
-    ret = recvfrom(sock_id, buf, sizeof(buf), 0, (struct sockaddr *)&server, &server_size); // получаем данные от сервера и записываем их в buf
+    ret = recvfrom(sock_id, buf, sizeof(buf), 0, (struct sockaddr*)&server, &server_size);
     if (ret < 0) {
         perror("Failed to recv data");
         exit(1);
     }
 
-
     std::cout << "Server reply: " << buf << std::endl;
+
+    close(sock_id);
 
     return 0;
 }
